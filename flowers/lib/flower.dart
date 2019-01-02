5095291b47c17a723c41ff256d82b35fb85e08d5
richie-south <richard.soderman@gmail.com>
2019-01-02 20:55:54 +0100

    feat: add more props for flower class

--- flowers/lib/flower.dart
+++ flowers/lib/flower.dart
@@ -1,10 +1,16 @@
 
 class Flower {
   String name;
+  String imageUrl;
+  String key;
   DateTime lastTimeWatered;
+  DateTime nextWaterTime;
 
   Flower({
     this.name,
-    this.lastTimeWatered
+    this.lastTimeWatered,
+    this.nextWaterTime,
+    this.imageUrl,
+    this.key
   });
 }
