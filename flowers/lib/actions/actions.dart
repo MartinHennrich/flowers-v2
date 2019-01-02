d8123b94a9ed6eb752b5c631e63286a3a645ad3f
richie-south <richard.soderman@gmail.com>
2019-01-02 20:56:34 +0100

    feat: flowers and fetch status actions

--- flowers/lib/actions/actions.dart
+++ flowers/lib/actions/actions.dart
@@ -4,3 +4,13 @@ class AddFlowerAction {
   Flower flower;
   AddFlowerAction(this.flower);
 }
+
+class AddFlowersAction {
+  List<Flower> flowers;
+  AddFlowersAction(this.flowers);
+}
+
+enum FetchingData {
+  Completed,
+  Fetching
+}
