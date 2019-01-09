d60ef29b01aee8161bf1003927e7d65aca110103
richie-south <richard.soderman@gmail.com>
2019-01-09 19:16:23 +0100

    feat: empty list widget for flower list page

---
+++ flowers/lib/components/flowers-list-page/emptyList.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+import '../../presentation/custom_icons_icons.dart';
+
+class EmptyList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 68, 0, 68),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            CustomIcons.emo_displeased,
+            color: YellowMain,
+            size: 70,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 28),
+            child: Text(
+              'add flowers to start you journey',
+              style: TextStyle(
+                color: Color.fromRGBO(0, 0, 0, 0.3)
+              ),
+            )
+          ),
+        ],
+      )
+    );
+  }
+}
