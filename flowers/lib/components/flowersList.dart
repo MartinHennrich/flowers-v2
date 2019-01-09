70b4d492fef7c9f4ebce3d28cc22997d3a472315
richie-south <richard.soderman@gmail.com>
2019-01-09 19:13:50 +0100

    feat: generic flowers list

--- flowers/lib/components/flowersList.dart
+++ flowers/lib/components/flowersList.dart
@@ -1,34 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-import '../../flower.dart';
-import '../../constants/colors.dart';
-import '../../utils/flowerHelpers.dart';
-import './water-dialog/waterDialog.dart';
-import '../../utils/colors.dart';
+import '../flower.dart';
+import '../constants/colors.dart';
+import '../utils/flowerHelpers.dart';
+
+import '../utils/colors.dart';
 
 class FlowersList extends StatelessWidget {
   final List<Flower> flowers;
   final bool disabled;
+  final Function(Flower flower) onPress;
 
   FlowersList({
     this.flowers,
-    this.disabled = false
+    this.onPress,
+    this.disabled = false,
   });
+
+  LinearGradient _getColor(Flower flower) {
+
+    if (disabled) {
+      return BlueGradient;
+    }
+
+    return getColorBasedOnTime(flower.nextWaterTime);
+  }
 
   List<Widget> _getFlowersListWidgets(List<Flower> flowers, BuildContext context) {
     return flowers.map((flower) {
+      Color timeBasedColor = getColorBasedOnTime2(flower.nextWaterTime, flower.lastTimeWatered);
+      Color _kKeyUmbraOpacity = timeBasedColor.withAlpha(51);
+      Color _kKeyPenumbraOpacity = timeBasedColor.withAlpha(36);
+      Color _kAmbientShadowOpacity =timeBasedColor.withAlpha(31);
+
       return GestureDetector(
         onTap: disabled ? null : () {
-          print('watering');
-          showDialog(
-            context: context,
-            builder: (_) => WaterDialog(flower: flower)
-          );
+          onPress(flower);
         },
         child: Container(
-          width: 160,
-          height: 190,
+          width: 144,
+          height: 170,
           margin: EdgeInsets.fromLTRB(0, 16, 16, 0),
           decoration: BoxDecoration(
             color: Colors.black12,
@@ -46,10 +58,13 @@ class FlowersList extends StatelessWidget {
                 alignment: Alignment(-1.0, 0.0),
                 padding: EdgeInsets.only(left: 16),
                 decoration: BoxDecoration(
-                  gradient: disabled
-                    ? BlueGradient
-                    : getColorBasedOnTime(flower.nextWaterTime),
+                  gradient: _getColor(flower),
                   borderRadius: BorderRadius.vertical(bottom: Radius.circular(8.0)),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(offset: Offset(0.0, 3.0), blurRadius: 3.0, spreadRadius: -2.0, color: _kKeyUmbraOpacity),
+                    BoxShadow(offset: Offset(0.0, 3.0), blurRadius: 4.0, spreadRadius: 0.0, color: _kKeyPenumbraOpacity),
+                    BoxShadow(offset: Offset(0.0, 1.0), blurRadius: 8.0, spreadRadius: 0.0, color: _kAmbientShadowOpacity),
+                  ],
                 ),
                 child: Text(
                   flower.name,
