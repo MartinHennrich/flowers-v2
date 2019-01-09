3fb6de5d40f2c30e0f80d5b33641e88b85a193cd
richie-south <richard.soderman@gmail.com>
2019-01-09 19:15:02 +0100

    feat: remove unused colors

--- flowers/lib/constants/colors.dart
+++ flowers/lib/constants/colors.dart
@@ -1,34 +1,15 @@
 import 'package:flutter/material.dart';
 
-const MainColor = Color.fromRGBO(57, 111, 106, 1);
-const MainSecondColor = Color.fromRGBO(79, 160, 147, 1);
+const GreenMain = Color.fromRGBO(22, 227, 167, 1);
+const GreenSecondMain = Color.fromRGBO(49, 243, 211, 1);
 
-const SecondMainColor = Color.fromRGBO(246, 194, 195, 1);
-const SecondSecondColor = Color.fromRGBO(245, 233, 228, 1);
-
-const PurpleMain = Color.fromRGBO(124, 156, 248, 1);
-const PurpleSecond = Color.fromRGBO(201, 158, 247, 1);
-
-const PurpleGradient = LinearGradient(
+const GreenGradient = LinearGradient(
   begin: Alignment.topLeft,
   end: Alignment.bottomRight,
-  stops: [0.1, 0.9],
+  stops: [0, 0.9],
   colors: [
-    PurpleMain,
-    PurpleSecond
-  ],
-);
-
-const GreenBlueMain = Color.fromRGBO(22, 227, 167, 1);
-const GreenBlueSecond = Color.fromRGBO(0, 169, 239, 1);
-
-const GreenBlueGradient = LinearGradient(
-  begin: Alignment.topLeft,
-  end: Alignment.bottomRight,
-  stops: [0.1, 0.9],
-  colors: [
-    GreenBlueMain,
-    GreenBlueSecond
+    GreenMain,
+    GreenSecondMain
   ],
 );
 
@@ -58,19 +39,6 @@ const YellowGradient = LinearGradient(
   ],
 );
 
-const RedPinkMain = Color.fromRGBO(249, 77, 113, 1);
-const RedPinkSecond = Color.fromRGBO(245, 47, 97, 1);
-
-const RedPinkGradient = LinearGradient(
-  begin: Alignment.topLeft,
-  end: Alignment.bottomRight,
-  stops: [0.1, 0.9],
-  colors: [
-    RedPinkMain,
-    RedPinkSecond
-  ],
-);
-
 const RedMain = Color.fromRGBO(255, 99, 88, 1);
 const RedSecond = Color.fromRGBO(254, 74, 76, 1);
 
@@ -84,11 +52,3 @@ const RedGradient = LinearGradient(
   ],
 );
 
-const GreyGradient = LinearGradient(
-  begin: Alignment.topLeft,
-  end: Alignment.bottomRight,
-  stops: [1],
-  colors: [
-    Colors.black12,
-  ],
-);
