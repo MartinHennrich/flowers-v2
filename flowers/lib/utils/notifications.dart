420d1de72ca7e2758fd06c2aff614c9de1745aba
richie-south <richard.soderman@gmail.com>
2019-01-10 18:43:56 +0100

    feat: handle notifications

---
+++ flowers/lib/utils/notifications.dart
@@ -0,0 +1,75 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../constants/colors.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+  'flowers.richardsoderman.com', 'flowers', 'Reminder to water your flowers',
+  color: BlueMain,
+  groupKey: 'water-time',
+  importance: Importance.Low,
+  priority: Priority.Low
+);
+
+NotificationDetails platformChannelSpecifics = NotificationDetails(
+  androidPlatformChannelSpecifics,
+  null
+);
+
+void initNotifications() {
+  var initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+  var initializationSettings = InitializationSettings(
+    initializationSettingsAndroid, null
+  );
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
+}
+
+void cancelOldNotification(String key, SharedPreferences prefs) {
+  List<String> keys = (prefs.getStringList('$key-notifications') ?? []);
+  List<int> intKeys = keys.map((key) => int.parse(key)).toList();
+
+  intKeys.forEach((key) {
+    flutterLocalNotificationsPlugin.cancel(key);
+  });
+}
+
+Future<void> saveNotificationKeys(String key, List<int> keys SharedPreferences prefs) async {
+  await prefs.setStringList('$key-notifications', keys.map((v) => v.toString()).toList());
+}
+
+Future<void> scheduleNotification(String key, String name, DateTime time) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  cancelOldNotification(key, prefs);
+
+  DateTime notificationTime = DateTime(time.year, time.month, time.day, 8);
+  DateTime reminderTime = notificationTime.add(Duration(days: 1));
+
+  int notisKey = key.hashCode;
+  int reminderKey = key.hashCode+1;
+
+  Future.wait([
+    flutterLocalNotificationsPlugin.schedule(
+      notisKey,
+      'Flower water reminder', '$name wants water',
+      notificationTime,
+      platformChannelSpecifics,
+      payload: key
+    ),
+    flutterLocalNotificationsPlugin.schedule(
+      reminderKey,
+      'Flower water reminder', 'Reminder: $name nees water now!',
+      reminderTime,
+      platformChannelSpecifics,
+      payload: key
+    )
+  ]);
+
+  saveNotificationKeys(key, [notisKey, reminderKey], prefs);
+}
+
+Future<void> rescheduleNotification(String key, name, DateTime time) async {
+  scheduleNotification(key, name, time);
+}
