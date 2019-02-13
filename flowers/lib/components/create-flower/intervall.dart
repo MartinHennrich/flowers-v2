c7cb3d6b28914ff5a30e7f49af6fb207da215274
richie-south <richard.soderman@gmail.com>
2019-02-13 20:23:34 +0100

    refactor: make more generic

--- flowers/lib/components/create-flower/intervall.dart
+++ flowers/lib/components/create-flower/intervall.dart
@@ -2,13 +2,97 @@ import 'package:flutter/material.dart';
 
 import '../../constants/colors.dart';
 
+class IntervallPure extends StatelessWidget {
+  final Function(int) onIncrease;
+  final Function(int) onDecrease;
+  final int value;
+  final String type;
+
+  IntervallPure({
+    @required this.onIncrease,
+    @required this.onDecrease,
+    @required this.value,
+    this.type = ''
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 16),
+        padding: EdgeInsets.only(top: 16),
+        child: Column(
+          children: <Widget>[
+            Text('$type every',
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16
+              )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  alignment: Alignment(0, 0),
+                  iconSize: 44,
+                  color: Colors.black26,
+                  onPressed: () {
+                    if (value - 1 >= 1) {
+                      onDecrease(value - 1);
+                    }
+                  },
+                  icon: Icon(
+                    Icons.keyboard_arrow_left,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(16, 12, 16, 12),
+                  child: Text(
+                    value.toString(),
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: BlueMain
+                    ),
+                  ),
+                ),
+                IconButton(
+                  alignment: Alignment(0, 0),
+                  iconSize: 44,
+                  color: Colors.black26,
+                  onPressed: () {
+                    onDecrease(value + 1);
+                  },
+                  icon: Icon(
+                    Icons.keyboard_arrow_right,
+                  ),
+                )
+              ],
+            ),
+            Text('days',
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16
+              )
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
+
+
 class Intervall extends StatelessWidget {
   final Function(int) onSave;
+  final Function(int) onChange;
   final int initialValue;
+  String type;
 
   Intervall({
     @required this.onSave,
-    this.initialValue = 5
+    this.initialValue = 5,
+    this.type,
+    this.onChange
   });
 
   @override
@@ -19,68 +103,23 @@ class Intervall extends StatelessWidget {
         onSave(form);
       },
       builder: (intervallFrom) {
-        return Center(
-          child: Container(
-            margin: EdgeInsets.only(top: 16),
-            padding: EdgeInsets.only(top: 16),
-            child: Column(
-              children: <Widget>[
-                Text('Water frequency',
-                  style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 16
-                  )
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    IconButton(
-                      alignment: Alignment(0, 0),
-                      iconSize: 44,
-                      color: Colors.black26,
-                      onPressed: () {
-                        if (intervallFrom.value - 1 >= 1) {
-                          intervallFrom.setValue(intervallFrom.value - 1);
-                          intervallFrom.setState((){});
-                        }
-                      },
-                      icon: Icon(
-                        Icons.keyboard_arrow_left,
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(16, 12, 16, 12),
-                      child: Text(
-                        intervallFrom.value.toString(),
-                        style: TextStyle(
-                          fontSize: 40,
-                          color: BlueMain
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      alignment: Alignment(0, 0),
-                      iconSize: 44,
-                      color: Colors.black26,
-                      onPressed: () {
-                        intervallFrom.setValue(intervallFrom.value + 1);
-                        intervallFrom.setState((){});
-                      },
-                      icon: Icon(
-                        Icons.keyboard_arrow_right,
-                      ),
-                    )
-                  ],
-                ),
-                Text('days',
-                  style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 16
-                  )
-                ),
-              ],
-            )
-          )
+        return IntervallPure(
+          value: intervallFrom.value,
+          onDecrease: (value) {
+            if (onChange != null) {
+              onChange(value);
+            }
+            intervallFrom.setValue(value);
+            intervallFrom.setState((){});
+          },
+          onIncrease: (value) {
+            if (onChange != null) {
+              onChange(value);
+            }
+            intervallFrom.setValue(value);
+            intervallFrom.setState((){});
+          },
+          type: type,
         );
       },
     );
