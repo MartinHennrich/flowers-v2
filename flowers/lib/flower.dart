9e38da31c5646800c2d98e1daf2b0d8c1e49c26a
richie-south <richard.soderman@gmail.com>
2019-02-13 20:25:41 +0100

    feat: add isActive and other helper functions

--- flowers/lib/flower.dart
+++ flowers/lib/flower.dart
@@ -20,6 +20,7 @@ enum ReminderType {
 
 class Reminder {
   String key;
+  bool isActive;
   ReminderType type;
   int interval;
   DateTime lastTime;
@@ -33,6 +34,7 @@ class Reminder {
     this.nextTime,
     this.timeOfDayForNotification,
     this.type,
+    this.isActive = true,
   });
 }
 
@@ -47,9 +49,43 @@ class Reminders {
     this.rotate
   });
 
+  List<Reminder> getRemindersAsList() {
+    List<Reminder> list = [];
+
+    if (water != null) {
+      list.add(water);
+    }
+
+    if (fertilize != null) {
+      list.add(fertilize);
+    }
+
+    if (rotate != null) {
+      list.add(rotate);
+    }
+
+    return list.expand((i) => [i]).toList();
+  }
+
+  void updateReminder(Reminder reminder) {
+    switch (reminder.type) {
+      case ReminderType.Water:
+        water = reminder;
+      break;
+      case ReminderType.Fertilize:
+        fertilize = reminder;
+      break;
+      case ReminderType.Rotate:
+        rotate = reminder;
+      break;
+      default:
+      break;
+    }
+  }
+
   Map<String, dynamic> toFirebaseObject() {
     Map<String, dynamic> obj = {};
-    if (water != null) {
+    if (water != null && water.isActive) {
       obj['water'] = {
         'interval': water.interval,
         'lastTime': water.lastTime.toIso8601String(),
@@ -57,7 +93,7 @@ class Reminders {
         'timeOfDayForNotification': water.timeOfDayForNotification.toIso8601String()
       };
     }
-    if (fertilize != null) {
+    if (fertilize != null && fertilize.isActive) {
       obj['fertilize'] = {
         'interval': fertilize.interval,
         'lastTime': fertilize.lastTime.toIso8601String(),
@@ -65,7 +101,7 @@ class Reminders {
         'timeOfDayForNotification': fertilize.timeOfDayForNotification.toIso8601String()
       };
     }
-    if (rotate != null) {
+    if (rotate != null && rotate.isActive) {
       obj['rotate'] = {
         'interval': rotate.interval,
         'lastTime': rotate.lastTime.toIso8601String(),
