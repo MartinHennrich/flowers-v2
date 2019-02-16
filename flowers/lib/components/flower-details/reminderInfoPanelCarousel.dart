5e6fff05533eae6b8e8017622fdbb8a52395aa47
richie-south <richard.soderman@gmail.com>
2019-02-16 09:24:09 +0100

    feat: show a carousel of reminder info panels one for each active reminder

---
+++ flowers/lib/components/flower-details/reminderInfoPanelCarousel.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+import '../../flower.dart';
+import './reminderInfoPanel.dart';
+
+class ReminderInfoPanelCarousel extends StatelessWidget {
+  final List<Reminder> reminders;
+
+  ReminderInfoPanelCarousel({
+    this.reminders,
+  });
+
+  Widget _getEmptyRemindersWidget() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 24, 0, 8),
+      alignment: Alignment(0, 0),
+      child: Text('NO ACTIVE REMINDERS :\'( ',
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.black38
+        ),
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    if (reminders.length == 0) {
+      return _getEmptyRemindersWidget();
+    }
+
+    if (reminders.length == 1) {
+      return ReminderInfoPanel(
+        reminder: reminders[0],
+        sidePadding: 16,
+      );
+    }
+
+    return CarouselSlider(
+      items: reminders.map((reminder) {
+        return Builder(
+          builder: (BuildContext context) {
+            return ReminderInfoPanel(
+              reminder: reminder,
+            );
+          },
+        );
+      }).toList(),
+      height: 106.0,
+      autoPlay: true,
+      viewportFraction: 0.9,
+    );
+  }
+}
