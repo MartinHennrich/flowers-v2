1fb6b8828b484e917a1b1664f3e2e4668337bcfd
richie-south <richard.soderman@gmail.com>
2019-02-16 18:07:41 +0100

    feat: avaiable reminders in app

---
+++ flowers/lib/constants/reminders.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import './colors.dart';
+import '../presentation/custom_icons_icons.dart';
+import '../flower.dart';
+
+class AvaiableReminder {
+  final String title;
+  final IconData iconData;
+  final Color color;
+  final ReminderType reminderType;
+
+  AvaiableReminder(
+    this.title,
+    this.iconData,
+    this.color,
+    this.reminderType
+  );
+}
+
+List<AvaiableReminder> avaiableReminders = [
+  AvaiableReminder('water', CustomIcons.water_amount_small, ReminderBlueMain, ReminderType.Water),
+  AvaiableReminder('fertilize', Icons.flash_on, BrownMain, ReminderType.Fertilize),
+  AvaiableReminder('rotate', Icons.rotate_left, YellowMain, ReminderType.Rotate)
+];
+
