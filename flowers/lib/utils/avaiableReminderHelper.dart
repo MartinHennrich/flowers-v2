2f113d84d0701f62c7379ad2922da4a4ef9635a9
richie-south <richard.soderman@gmail.com>
2019-02-17 10:48:20 +0100

    feat: create avaiable reminder object from reminders

---
+++ flowers/lib/utils/avaiableReminderHelper.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+import '../presentation/custom_icons_icons.dart';
+import '../constants/reminders.dart';
+import '../flower.dart';
+
+List<AvaiableReminder> getAvaiableReminderFromReminders(Reminders reminders) {
+  List<Reminder> reminderItems = reminders.getRemindersAsList();
+
+  List<AvaiableReminder> created = reminderItems.map((reminder) {
+    switch (reminder.type) {
+      case ReminderType.Water:
+        return AvaiableReminder('water', CustomIcons.water_amount_small, ReminderBlueMain, ReminderType.Water);
+      case ReminderType.Fertilize:
+        return AvaiableReminder('fertilize', Icons.flash_on, BrownMain, ReminderType.Fertilize);
+      case ReminderType.Rotate:
+        return AvaiableReminder('rotate', Icons.rotate_left, YellowMain, ReminderType.Rotate);
+      default:
+        return AvaiableReminder('none', Icons.warning, RedMain, ReminderType.Water);
+    }
+  }).toList();
+
+  return created;
+}
