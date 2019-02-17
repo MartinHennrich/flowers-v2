0525499479c9d5614a36c3d7be69b1c4ec13c333
richie-south <richard.soderman@gmail.com>
2019-02-17 10:46:09 +0100

    feat: add reminders in detail view page

---
+++ flowers/lib/components/flower-details/add-reminders/addRemindersPage.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/firebase.dart';
+import '../../../actions/actions.dart';
+import '../../../flower.dart';
+import '../../../store.dart';
+import '../../../constants/colors.dart';
+import '../../create-flower/addReminders.dart';
+import '../../../utils/avaiableReminderHelper.dart';
+import '../../../utils/notifications.dart';
+
+class AddRemindersPage extends StatefulWidget {
+  final Flower flower;
+  final Reminders reminders;
+
+  AddRemindersPage({
+    this.flower,
+    this.reminders
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return AddRemindersPageState();
+  }
+}
+
+class AddRemindersPageState extends State<AddRemindersPage> {
+  final _formKey = GlobalKey<FormState>();
+  Reminders remindersFormData = Reminders();
+  bool isSaving = false;
+
+  @override
+  void initState() {
+    super.initState();
+    remindersFormData = widget.reminders;
+  }
+
+  void _onAddReminders() async {
+    Flower flower = widget.flower;
+    flower.reminders = remindersFormData;
+
+    setState(() {
+      isSaving = true;
+    });
+    try {
+      await database.updateflower(flower);
+      AppStore.dispatch(UpdateFlowerAction(flower));
+      Navigator.pop(context);
+
+      scheduleNotificationsForReminders(flower.name, remindersFormData);
+    } catch (e) {
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('ADD REMINDERS'),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: !isSaving ? () {
+              _formKey.currentState.save();
+              _onAddReminders();
+            } : null,
+            child: Text('SAVE'),
+          )
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AddReminders(
+                onSave: (Reminders reminders) {
+                  remindersFormData = reminders;
+                },
+                defaultReminders: widget.reminders,
+                defaultCreatedReminders: getAvaiableReminderFromReminders(widget.reminders),
+              ),
+            ]
+          )
+        )
+      )
+    );
+  }
+}
