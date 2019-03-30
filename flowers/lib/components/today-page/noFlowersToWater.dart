a10672aa65804fbfd9efe116a16f192b33c36573
richie-south <richard.soderman@gmail.com>
2019-03-30 08:40:13 +0100

    fix: sentence

--- flowers/lib/components/today-page/noFlowersToWater.dart
+++ flowers/lib/components/today-page/noFlowersToWater.dart
@@ -33,7 +33,7 @@ class NoFlowersToWater extends StatelessWidget {
       return 'add plants\nto start you journey';
     }
 
-    return 'no plants need water';
+    return 'no plants need care';
   }
 
   Color _getColor() {
