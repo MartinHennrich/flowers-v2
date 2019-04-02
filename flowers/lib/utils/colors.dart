a4f9c76f52cca2542682f3ddfba523f976e3977c
richie-south <richard.soderman@gmail.com>
2019-04-02 19:06:38 +0200

    feat: calculate color from flower

--- flowers/lib/utils/colors.dart
+++ flowers/lib/utils/colors.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../constants/colors.dart';
 import './dateHelpers.dart';
+import '../flower.dart';
+import '../reminders.dart';
 
 LinearGradient getColorGradientBasedOnTime(DateTime time, DateTime lastTime) {
   DateTime today = preSetTimeFrame(DateTime.now());
@@ -48,3 +50,37 @@ Color getColorBasedOnTime(DateTime time, DateTime lastTime) {
   return RedMain;
 }
 
+LinearGradient getColorGradientForFlower(Flower flower) {
+  List<Reminder> r = flower.reminders.getRemindersThatNeedAction(DateTime.now());
+  if (r.length == 0) {
+    var p = flower.reminders.getCompletedReminders(DateTime.now());
+    if (p.length > 0) {
+      return BlueGradient;
+    }
+  }
+
+  Reminder clostToDate = flower.reminders.getClosestDate(DateTime.now());
+  if (clostToDate == null) {
+    return GreenGradient;
+  }
+
+  return getColorGradientBasedOnTime(clostToDate.nextTime, clostToDate.lastTime);
+}
+
+Color getColorForFlower(Flower flower) {
+
+  List<Reminder> r = flower.reminders.getRemindersThatNeedAction(DateTime.now());
+  if (r.length == 0) {
+    var p = flower.reminders.getCompletedReminders(DateTime.now());
+    if (p.length > 0) {
+      return BlueMain;
+    }
+  }
+
+  Reminder clostToDate = flower.reminders.getClosestDate(DateTime.now());
+  if (clostToDate == null) {
+    return GreenMain;
+  }
+
+  return getColorBasedOnTime(clostToDate.nextTime, clostToDate.lastTime);
+}
