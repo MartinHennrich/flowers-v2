95dfb0f2cdaef1d4c4c0f11a53f1cdf91ee638d9
richie-south <richard.soderman@gmail.com>
2019-04-22 20:33:40 +0200

    feat: select labels and filter flowers on label

--- flowers/lib/components/flowers-list-page/flowersListPage.dart
+++ flowers/lib/components/flowers-list-page/flowersListPage.dart
@@ -11,8 +11,19 @@ import '../flower-details/flowerDetails.dart';
 import '../flowersList.dart';
 import '../page-title.dart';
 import './emptyList.dart';
+import '../flower-details/labels-list/labelsList.dart';
+import '../../utils/labelsHelper.dart';
 
-class FlowersListPage extends StatelessWidget {
+class FlowersListPage extends StatefulWidget {
+
+  @override
+  FlowersListPageState createState() {
+    return FlowersListPageState();
+  }
+}
+
+class FlowersListPageState extends State<FlowersListPage> {
+  List<Label> selectedLabels = [];
 
   List<Widget> _getLoadingScreen() {
     return [
@@ -50,6 +61,40 @@ class FlowersListPage extends StatelessWidget {
     Scaffold.of(context).showSnackBar(snackBar);
   }
 
+  List<Flower> _req(List<Flower> subFlowers, Label selectedLabel) {
+    List<Flower> filterFlowers = [];
+
+    subFlowers.forEach((flower) {
+      var v = flower.labels.firstWhere(
+        (label) => label.value == selectedLabel.value,
+        orElse: () => null
+      );
+
+      if (v != null) {
+        var hasFlower = filterFlowers.firstWhere(
+          (fFlower) => fFlower.key == flower.key,
+          orElse: () => null
+        );
+
+        if (hasFlower == null) {
+          filterFlowers.add(flower);
+        }
+      }
+    });
+
+    return filterFlowers;
+  }
+
+  List<Flower> _filterOnSelectedLabels(List<Flower> flowers) {
+    List<Flower> filterFlowers = flowers;
+
+    selectedLabels.forEach((selectedLabel) {
+      filterFlowers = _req(filterFlowers, selectedLabel);
+    });
+
+    return filterFlowers;
+  }
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector(
@@ -60,21 +105,62 @@ class FlowersListPage extends StatelessWidget {
       },
       converter: _ViewModel.fromStore,
       builder: (context, _ViewModel vm) {
-        List<Flower> sorted = _sortFlowersAlphabetically(vm.flowers);
+        List<Label> allLabels = getAllUniqLabels();
+        List<Flower> sorted = _sortFlowersAlphabetically(
+          selectedLabels.length > 0
+            ? _filterOnSelectedLabels(vm.flowers)
+            : vm.flowers
+        );
+
+        if (allLabels.length == 0 && selectedLabels.length > 0) {
+          setState(() {
+            selectedLabels = [];
+          });
+        }
 
         List<Widget> children = [
-          PageTitle(title: 'Garden'),
-          FlowersList(
-            flowers: sorted,
-            withHero: true,
-            onPress: (Flower flower) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => FlowerDetails(flower: flower),
-                ),
-              );
-            },
+          PageTitle(
+            title: 'Garden',
+            padding: EdgeInsets.fromLTRB(20, 44, 20, allLabels.length > 0 ? 10 : 40),
+          ),
+          allLabels.length > 0
+            ? Padding(
+              padding: EdgeInsets.only(bottom: 0),
+              child: LabelsList(
+                labels: getAllUniqLabels(),
+                selected: selectedLabels,
+                onLabelPress: (Label label) {
+                  if (selectedLabels.firstWhere(
+                    (selectedLabel) => selectedLabel.value == label.value,
+                    orElse: () => null) == null) {
+                      setState(() {
+                        selectedLabels.add(label);
+                      });
+                  } else {
+                    setState(() {
+                      selectedLabels = selectedLabels.where(
+                        (selectedLabel) => selectedLabel.value != label.value
+                      ).toList();
+                    });
+                  }
+                },
+              ),
+            )
+            : Container(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: FlowersList(
+              flowers: sorted,
+              withHero: true,
+              onPress: (Flower flower) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FlowerDetails(flower: flower),
+                  ),
+                );
+              },
+            ),
           ),
           vm.flowers.length == 0
             ? EmptyList()
@@ -84,7 +170,7 @@ class FlowersListPage extends StatelessWidget {
         return Scaffold(
           backgroundColor: Colors.white,
           body: CustomScrollColor(child: ListView(
-            padding: EdgeInsets.all(20.0),
+            padding: EdgeInsets.only(bottom: 20),
             children: vm.isFetchingData == true ? _getLoadingScreen() : children
           ),
         ));
