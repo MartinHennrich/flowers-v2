4301c00bcd7bb1a90d0081a0733499effe0fbfb8
richie-south <richard.soderman@gmail.com>
2019-04-24 17:11:15 +0200

    feat: make empty list more dynamic

--- flowers/lib/components/flowers-list-page/emptyList.dart
+++ flowers/lib/components/flowers-list-page/emptyList.dart
@@ -4,6 +4,15 @@ import '../../constants/colors.dart';
 import '../../presentation/custom_icons_icons.dart';
 
 class EmptyList extends StatelessWidget {
+  final Color color;
+  final IconData iconData;
+  final String text;
+
+  EmptyList({
+    this.iconData,
+    this.color,
+    this.text,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +22,14 @@ class EmptyList extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Icon(
-            CustomIcons.emo_sunglasses,
-            color: YellowMain,
+            iconData,
+            color: color,
             size: 70,
           ),
           Padding(
             padding: EdgeInsets.only(top: 28),
             child: Text(
-              'add plants\nto start you journey',
+              text,
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Color.fromRGBO(0, 0, 0, 0.3)
